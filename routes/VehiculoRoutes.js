d47ae41853db7a27075b57fe2cb61494c7d49977
Hanselespino <hansomgonzalez@gmail.com>
2024-11-30 19:12:59 -0600

    Modificacion Commit

--- routes/VehiculoRoutes.js
+++ routes/VehiculoRoutes.js
@@ -2,9 +2,9 @@ const VehiculoController = require('../controllers/VehiculoController');
 const auth = require('../auth/auth');
 
 module.exports = (app) => {
-    app.get('/vehiculo/:id', auth.authenticate,VehiculoController.getAllByUserIdVehiculos);
-    app.get('/vehiculo/:id/tipo', auth.authenticate,VehiculoController.getAllByUserIdVehiculosTipo);
-    app.post('/vehiculo', auth.authenticate, VehiculoController.createvehiculo);
-    app.delete('/vehiculo/:id', auth.authenticate,VehiculoController.deletedVehiculo);
+    app.get('/vehiculo/:id', VehiculoController.getAllByUserIdVehiculos);
+    app.get('/vehiculo/:id/tipo', VehiculoController.getAllByUserIdVehiculosTipo);
+    app.post('/vehiculo',  VehiculoController.createvehiculo);
+    app.delete('/vehiculo/:id', VehiculoController.deletedVehiculo);
 }
 
